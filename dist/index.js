1ff35ac9f58d8c086c27669e0baed0fe0a7d321d
Geeballer <jibola422@gmail.com>
2023-03-17 15:40:52 -0700

    First commit-

---
+++ dist/index.js
@@ -0,0 +1,72 @@
+/**********************************************
+                PRELOADER
+ **********************************************/
+const preloader = document.querySelector('.preloader')
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        preloader.classList.add('invisible')
+    }, 3000)
+})
+
+/**********************************************
+                CAROUSEL SLIDE
+ **********************************************/
+const slider = document.querySelector('.slider'),
+      slideList = document.querySelector('slide-wrapper'),
+      slideItems = document.querySelectorAll('.slide-list'),
+      prev = document.querySelector('.prev'),
+      next = document.querySelector('.next'),
+      sliderWidth = slider.offsetWidth,
+      count = 1;
+
+const responsiveSlider = () => {
+    
+    window.addEventListener('resize', () => {
+        sliderWidth = slider.offsetWidth
+    })
+
+    const prevSlide = () => {
+        if (count > 1){
+            count = count - 2
+            slideList.style.left = '_' + count * sliderWidth + 'px'
+            count++
+        }
+        else if (count == 1){
+            count = slideItems - 1
+            slideList.style.left = '_' + count * sliderWidth + 'px'
+            count++
+        }
+    }
+
+    const nextSlide = () => {
+        if (count < slideItems){
+            slideList.style.left = '_' + count * sliderWidth + 'px'
+            count++
+        }
+        else if (count == slideItems){
+            slideList.style.left = '0px'
+            count = 1
+        }
+    }
+
+    next.addEventListener('click', () => {
+        nextSlide()
+    })
+
+    prev.addEventListener('click', () => {
+        prevSlide()
+    })
+
+    setInterval(() => {
+        nextSlide()
+    }, 5000)
+}
+
+window.onload = () => {
+    responsiveSlider()
+}
+
+  /********************** Initialize WOW ********************/
+
+  new WOW().init();
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '400px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      nunito: ['"Nunito"', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        textColor: '#fff',
+        primaryColor: '#00020d',
+        secondaryColor: '#246bf6',
+        tertiaryColor: '#121b23',
+        bgTable: 'rgba(126, 126, 126, 0.164)'
+      }
+    },
+  },
+  plugins: [],
+}
